507cec52e7adc40ae828b35cf0f085c3084a672f
sqrmyy <dn41.ra20adtdjw@gmail.com>
2022-01-08 20:56:47 +0900

    create formControl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HelloComponent]
 })
 export class AppModule { }
---
+++ src/app/hello/hello.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-hello',
+  templateUrl: './hello.component.html',
+  styleUrls: ['./hello.component.css'],
+})
+export class HelloComponent implements OnInit {
+  title: string;
+  message: string;
+  myControl: FormControl
+
+  constructor() { 
+  }
+
+  ngOnInit() {
+    this.title = 'Hello-app'
+    this.message = 'FormControlを使う'
+    this.myControl = new FormControl('ok.')
+  }
+
+  doClick() { 
+    this.message = '「' + this.myControl.value + '」と書きましたね。'
+  }
+}
