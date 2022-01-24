1eaeb7f89ff859ef3b779abf79d71ac833f90272
sqrmyy <dn41.ra20adtdjw@gmail.com>
2022-01-24 11:18:40 +0900

    generate component

--- projects/newapp/src/app/app.module.ts
+++ projects/newapp/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ShowComponent } from './show/show.component';
+import { AddComponent } from './add/add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ShowComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule,
